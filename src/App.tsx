507c1fd2707102f1f3d69dd11e36e6c30b2808a9
jinhyunji <jinhyunji@jinhyunjiui-MacBookAir.local>
2026-02-09 13:40:43 +0900

    chore: 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import { useState } from 'react'
-
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div>메인페이지입니다</div>;
+}
+
+export default HomePage;
