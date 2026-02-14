af42df15dff412bd23af04b45bc4b94c1820104f
nyeong <hannayoung1210@gmail.com>
2026-02-14 22:42:56 +0900

    feat:CartItem 목데이터 추가

---
+++ src/mocks/data/cartItems.ts
@@ -0,0 +1,25 @@
+import { CartItem } from "@/stores/cart.store";
+
+export const MOCK_CART_ITEMS: CartItem[] = [
+  {
+    productId: 1,
+    name: "먼작귀 키보드",
+    price: 89000,
+    quantity: 2,
+    imageUrl: "https://www.coupang.com/vp/products/8866904859",
+  },
+  {
+    productId: 2,
+    name: "귀여운 무선 게이밍 마우스",
+    price: 55000,
+    quantity: 1,
+    imageUrl: "https://www.instagram.com/p/C8TK_PISSWX/",
+  },
+  {
+    productId: 3,
+    name: "귀여운 장패드 (XL)",
+    price: 150000,
+    quantity: 5,
+    imageUrl: "https://www.wishbucket.io/items/1442362",
+  },
+];
