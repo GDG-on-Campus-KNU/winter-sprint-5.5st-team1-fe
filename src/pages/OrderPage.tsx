52bd96ce79b06922f2c353a7eee30deea01c2bc0
nyeong <hannayoung1210@gmail.com>
2026-02-14 22:20:29 +0900

    feat: 상품 요약 컴포넌트 적용 및 스타일&레이아웃 수정

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -3,14 +3,15 @@ import { cn } from "@/lib/utils";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
 import { Coupon } from "@/types/coupon";
 import { getAvailableCoupons } from "@/mocks/data/coupons";
 import { CouponCard } from "@/components/cards/couponCard";
+import { OrderSummaryCard } from "@/components/cards/orderSummaryCard";
 import { X } from "lucide-react";
 
 function OrderPage() {
     const orderPrice = 80000;
+    const shipping = 3000;
     const [name, setName] = useState("");
     const [phone, setPhone] = useState("");
     const [address, setAddress] = useState("");
@@ -23,6 +24,8 @@ function OrderPage() {
         const available = getAvailableCoupons(orderPrice);
         return [...available].sort((a, b) => a.id - b.id);
     }, [orderPrice]);
+    const discountAmount = selectedCoupon ? (orderPrice * selectedCoupon.discountRate) / 100 : 0;
+    const totalAmount = orderPrice + shipping - discountAmount;
 
     return (
         <div className="w-full min-h-screen bg-pink-500/3 py-10 px-[120px]">
@@ -30,83 +33,88 @@ function OrderPage() {
                 <h1 className="text-[32px] font-semibold text-gray-500 ml-2 mb-10 text-left">
                     주문서 작성
                 </h1>
-
-                <Card className="w-full pt-8 pb-10 px-8 border border-gray-100 shadow-sm bg-white rounded-xl text-left">
-                    <div className="flex flex-col gap-7  text-[24px]">
-                        <div className="text-[28px] font-semibold text-gray-500 ml-1 leading-none">
-                            배송지 정보
-                        </div>
-
-                        <div className="space-y-6">
-                            <div className="grid grid-cols-2 gap-6">
-                                <div className="space-y-4">
-                                    <Label className="font-medium text-gray-500 ml-1">받는 분<span className="text-red">*</span></Label>
-                                    <Input placeholder="이름을 입력해주세요" className="h-16 !text-[24px]" value={name}
-                                        onChange={(e) => setName(e.target.value)} />
+                <div className="flex flex-row items-start gap-14 w-full">
+                    <div className="flex-1 space-y-10">
+                        <Card className="w-full pt-8 pb-10 px-8 border border-gray-100 shadow-sm bg-white rounded-xl text-left">
+                            <div className="flex flex-col gap-7  text-[24px]">
+                                <div className="text-[28px] font-semibold text-gray-500 ml-1 leading-none">
+                                    배송지 정보
                                 </div>
-                                <div className="space-y-4">
-                                    <Label className="font-medium text-gray-500 ml-1">전화번호<span className="text-red">*</span></Label>
-                                    <Input placeholder="010-0000-0000" className="h-16 !text-[24px]" value={phone}
-                                        onChange={(e) => setPhone(e.target.value)} />
+
+                                <div className="space-y-6">
+                                    <div className="grid grid-cols-2 gap-6">
+                                        <div className="space-y-4">
+                                            <Label className="font-medium text-gray-500 ml-1">받는 분<span className="text-red">*</span></Label>
+                                            <Input placeholder="이름을 입력해주세요" className="h-16 !text-[24px]" value={name}
+                                                onChange={(e) => setName(e.target.value)} />
+                                        </div>
+                                        <div className="space-y-4">
+                                            <Label className="font-medium text-gray-500 ml-1">전화번호<span className="text-red">*</span></Label>
+                                            <Input placeholder="010-0000-0000" className="h-16 !text-[24px]" value={phone}
+                                                onChange={(e) => setPhone(e.target.value)} />
+                                        </div>
+                                    </div>
+
+                                    <div className="space-y-4">
+                                        <Label className="font-medium text-gray-500 ml-1">주소<span className="text-red">*</span></Label>
+                                        <Input placeholder="도로명 주소를 입력해주세요" className="h-16 !text-[24px]" value={address}
+                                            onChange={(e) => setAddress(e.target.value)} />
+                                    </div>
+
+                                    <div className="space-y-4">
+                                        <Label className="font-medium text-gray-500 ml-1">상세주소</Label>
+                                        <Input placeholder="상세 주소를 입력해주세요" className="h-16 !text-[24px]" />
+                                    </div>
+
+                                    <div className="space-y-4">
+                                        <Label className="font-medium text-gray-500 ml-1">배송 메시지</Label>
+                                        <textarea
+                                            value={deliveryMessage}
+                                            onChange={(e) => setDeliveryMessage(e.target.value)}
+                                            className="w-full min-h-[140px] p-4 rounded-md border border-pink-300 focus:ring-[3px] focus:ring-pink-400/30 focus:border-pink-400 outline-none transition-all resize-none placeholder:text-muted-foreground text-[24px]"
+                                            placeholder="배송 시 요청사항을 적어주세요"
+                                        />
+                                    </div>
                                 </div>
                             </div>
+                        </Card>
+                        <Card className="w-full pt-8 pb-10 px-8 border border-gray-100 shadow-sm bg-white rounded-xl text-left">
+                            <div className="flex flex-col gap-7">
+                                <div className="text-[28px] font-semibold text-gray-500 ml-1 leading-none">
+                                    쿠폰 적용
+                                </div>
 
-                            <div className="space-y-4">
-                                <Label className="font-medium text-gray-500 ml-1">주소<span className="text-red">*</span></Label>
-                                <Input placeholder="도로명 주소를 입력해주세요" className="h-16 !text-[24px]" value={address}
-                                    onChange={(e) => setAddress(e.target.value)} />
+                                <div className="flex flex-col">
+                                    <button
+                                        type="button"
+                                        onClick={() => setIsModalOpen(true)}
+                                        className={cn(
+                                            "flex items-center px-4 w-full h-16 rounded-md border text-[24px] shadow-xs transition-all outline-none bg-white",
+                                            "border-pink-300",
+                                            "hover:border-pink-400 hover:bg-pink-50/30 cursor-pointer",
+                                            "focus:ring-[3px] focus:ring-pink-400/30 focus:border-pink-400",
+                                            selectedCoupon
+                                                ? "text-gray-500 font-medium"
+                                                : "text-gray-300 font-regular"
+                                        )}
+                                    >
+                                        {selectedCoupon ? `${selectedCoupon.discountRate}% ${selectedCoupon.title}` : "사용할 쿠폰을 선택하세요."}
+                                    </button>
+                                </div>
                             </div>
-
-                            <div className="space-y-4">
-                                <Label className="font-medium text-gray-500 ml-1">상세주소</Label>
-                                <Input placeholder="상세 주소를 입력해주세요" className="h-16 !text-[24px]" />
-                            </div>
-
-                            <div className="space-y-4">
-                                <Label className="font-medium text-gray-500 ml-1">배송 메시지</Label>
-                                <textarea
-                                    value={deliveryMessage}
-                                    onChange={(e) => setDeliveryMessage(e.target.value)}
-                                    className="w-full min-h-[140px] p-4 rounded-md border border-pink-300 focus:ring-[3px] focus:ring-pink-400/30 focus:border-pink-400 outline-none transition-all resize-none placeholder:text-muted-foreground text-[24px]"
-                                    placeholder="배송 시 요청사항을 적어주세요"
-                                />
-                            </div>
-                        </div>
+                        </Card>
                     </div>
-                </Card>
-                <Card className="w-full pt-8 pb-10 px-8 border border-gray-100 shadow-sm bg-white rounded-xl text-left">
-                    <div className="flex flex-col gap-7">
-                        <div className="text-[28px] font-semibold text-gray-500 ml-1 leading-none">
-                            쿠폰 적용
-                        </div>
-
-                        <div className="flex flex-col">
-                            <button
-                                type="button"
-                                onClick={() => setIsModalOpen(true)}
-                                className={cn(
-                                    "flex items-center px-4 w-full h-16 rounded-md border text-[24px] shadow-xs transition-all outline-none bg-white",
-                                    "border-pink-300",
-                                    "hover:border-pink-400 hover:bg-pink-50/30 cursor-pointer",
-                                    "focus:ring-[3px] focus:ring-pink-400/30 focus:border-pink-400",
-                                    selectedCoupon
-                                        ? "text-gray-500 font-medium"
-                                        : "text-gray-300 font-regular"
-                                )}
-                            >
-                                {selectedCoupon ? `${selectedCoupon.discountRate}% ${selectedCoupon.title}` : "사용할 쿠폰을 선택하세요."}
-                            </button>
-                        </div>
+                    <div>
+                        <OrderSummaryCard
+                            subtotal={orderPrice}
+                            shipping={shipping}
+                            discount={discountAmount}
+                            total={totalAmount}
+                            isOrderValid={isOrderValid}
+                        />
                     </div>
-                </Card>
-                <div className="pt-4 flex items-center justify-center">
-                    <Button
-                        className="w-[300px] h-18 text-[28px] font-semibold rounded-xl"
-                        disabled={!isOrderValid}
-                    >
-                        주문하기
-                    </Button>
                 </div>
+
             </div>
             {isModalOpen && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center">
