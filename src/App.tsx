945a8ff718701cab5d11b2ab3a622d4d773b2e8b
Catsmanager <hyunhyun0096@naver.com>
2026-02-19 13:07:43 +0900

    feat: 장바구니 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layouts/MainLayout";
-import HomePage from "./pages/home/HomePage"
+import HomePage from "./pages/home/HomePage";
 import ProductPage from "./pages/product/ProductPage";
 import ProductDetailPage from "./pages/product/ProductDetailPage";
 import OrderPage from "./pages/OrderPage";
 import OrderCompletePage from "./pages/order/OrderCompletePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
@@ -15,7 +16,11 @@ function App() {
           <Route path="/product" element={<ProductPage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
           <Route path="/order" element={<OrderPage />} />
-          <Route path="/order/complete/:orderId" element={<OrderCompletePage />} />
+          <Route
+            path="/order/complete/:orderId"
+            element={<OrderCompletePage />}
+          />
+          <Route path="/cart" element={<CartPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
