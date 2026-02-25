a438a54d5c5389b86d9cdabf61dae3c806043729
symarieChoi <symarie.choi@gmail.com>
2026-02-25 14:22:36 +0900

    chore: 탠스택 쿼리 설치 및 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Layout } from "./components/layouts/MainLayout";
 import HomePage from "./pages/home/HomePage";
 import ProductPage from "./pages/product/ProductPage";
@@ -8,21 +10,26 @@ import OrderCompletePage from "./pages/order/OrderCompletePage";
 import ProductManagePage from "./pages/admin/ProductManagePage";
 import CartPage from "./pages/CartPage";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product" element={<ProductPage />} />
-          <Route path="/products/:id" element={<ProductDetailPage />} />
-          <Route path="/order" element={<OrderPage />} />
-          <Route path="/order/complete/:orderId" element={<OrderCompletePage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/admin/products/:id" element={<ProductManagePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/products/:id" element={<ProductDetailPage />} />
+            <Route path="/order" element={<OrderPage />} />
+            <Route path="/order/complete/:orderId" element={<OrderCompletePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/admin/products/:id" element={<ProductManagePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
